5104624e0717d72d469f727422a2b923e379adfa
Amjad C P <84246684+amjadcp@users.noreply.github.com>
2022-10-10 12:11:44 +0530

    Update S2T.py

--- S2T.py
+++ S2T.py
@@ -1,6 +1,10 @@
 import speech_recognition as sr # need the module SpeechRecognition, pyAudio install it by(pip install  SpeechRecognition, pyAudio)
+import pyttsx3
+# python3 -m speech_recognition - find threshold
 
 listener = sr.Recognizer()
+listener.energy_threshold = 11115.33955353187 
+listener.dynamic_energy_threshold = True
 friend = pyttsx3.init()
 while True:
  with sr.Microphone() as source:
